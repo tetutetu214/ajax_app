136225fc0bfe38b5791c6956c95251bf38078166
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-10-27 16:40:02 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+  
   def create
     # Post.create(content: params[:content])
     # redirect_to action: :index
