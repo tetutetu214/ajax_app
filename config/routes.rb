793d937ca1a94b780c60fda9fc2d50d13405341d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-10-27 11:13:40 +0900

    AjaxApp準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
- get 'posts', to: 'posts#index'
- get 'posts/new', to: 'posts#new'
+ root to: "posts#index"
  post 'posts' , to: 'posts#create'
 
- #  root to: "posts#index
-#  resorces :post, only:[:index, :new, :create]
 end
   
